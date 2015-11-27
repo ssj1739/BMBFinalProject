36ba1367708a114e6ef947ae7d32163dbade19c5
warren <we270@nyu.edu>
2015-11-27 16:47:55 -0500

    added script
    script is not complete; needs work on gse6575 and needs work on getting
    probe names for gse7316

---
+++ get_gse6575_gse7316.r
@@ -0,0 +1,58 @@
+source("http://bioconductor.org/biocLite.R")
+biocLite("simpleaffy")
+biocLite("GEOquery")
+biocLite("affy")
+library(simpleaffy)
+library(GEOquery)
+library(affy)
+
+#getting GSE6575, "Gene expression in blood of children with autism spectrum disorder"
+
+#download.file('http://www.ncbi.nlm.nih.gov/geo/download/?acc=GSE6575&format=file', 'gse6575.tar', mode = 'wb')
+untar('gse6575.tar', exdir = 'gse6575') #odd error while downloading - check if files are corrupted?
+
+#get the whole normalized dataset
+setwd('gse6575')
+raw_6575 <- ReadAffy(compress = TRUE, verbose = TRUE)
+norm_6575 <- gcrma(raw_6575)
+exprs_6575 <- exprs(norm_6575)
+
+#get the experimental groups
+geo_6575 <- getGEO("GSE6575")
+exprs_geo_6575 <- exprs(geo_6575[[1]])
+file_designations = colnames(exprs_geo_6575)
+design_6575 = c()
+for (i in 1:length(file_designations)) {
+  gsm = getGEO(file_designations[i])
+  design_6575[i] = Meta(gsm)$characteristics_ch1
+}
+
+####
+
+#getting GSE7329, "Gene expression profiles of lymphoblastoid cells (autism)"
+
+#it appears that when you dowload GSE7316_RAW.tar from GEO, you actually get a bunch of txt.gz files, which are already normalized. So these are not 'raw' files. Let's skip getting the data / normalizing and just jump ahead with GEOquery.
+
+setwd('~/r/final_proj_affy_data')
+
+geo_7329 <- getGEO('GSE7329')
+exprs_geo_7329 <- exprs(geo_7329[[1]])
+
+#get the experimental conditions for ANOVA?
+file_designations_2 = colnames(exprs_geo_7329)
+design_7329 = c(rep('autism_dup15',7),rep('autism_fmr1',8),rep('control',15))
+#for (i in 1:length(file_designations_2)) {
+ # gsm = getGEO(file_designations_2[i])
+  #design_7329[i] = strsplit(Meta(gsm)$title, split=" ")[[1]][1]
+#}
+
+#get differentially expressed genes with ANOVA (function copied from Manny's SeedDevelopment lecture)
+doAnova<-function(expvalues, expgroups) {
+  anova.results = anova(lm(as.numeric(expvalues)
+                           ~ as.factor(expgroups)))$"Pr(>F)"[1]
+  anova.results
+}
+
+anova_7329 <- apply(exprs_geo_7329, 1, doAnova, design_7329) #returns ~44,000 elements
+test <- which(anova_7329 < .05)
+length(test) # ~10,000 elements with signficant p-values, let's see if we can narrow this down somehow
