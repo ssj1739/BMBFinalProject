3a757d952b6c8f34aaadf60df71fc83b56cf2d46
sjain <sidharthsjain@gmail.com>
2015-12-02 00:11:08 -0500

    Added script for additional GSE files and further analysis

--- get_gse6575_gse7316.r
+++ get_gse6575_gse7316.r
@@ -1,15 +1,19 @@
+# Final Project
+
 source("http://bioconductor.org/biocLite.R")
+
+# load all packages
 biocLite("simpleaffy")
 biocLite("GEOquery")
 biocLite("affy")
+
 library(simpleaffy)
 library(GEOquery)
 library(affy)
 
 wd <- "~/BMBFinalProject"
 
-#getting GSE6575, "Gene expression in blood of children with autism spectrum disorder"
-#getting GSE
+# Getting GSE6575, "Gene expression in blood of children with autism spectrum disorder"
 
 #download.file('http://www.ncbi.nlm.nih.gov/geo/download/?acc=GSE6575&format=file', 'gse6575.tar', mode = 'wb')
 untar('gse6575.tar', exdir = 'gse6575') #odd error while downloading - check if files are corrupted?
@@ -30,16 +34,19 @@ for (i in 1:length(file_designations)) {
   design_6575[i] = Meta(gsm)$characteristics_ch1
 }
 
-####
+
+#########################################
 
 #getting GSE7329, "Gene expression profiles of lymphoblastoid cells (autism)"
 
-#it appears that when you dowload GSE7316_RAW.tar from GEO, you actually get a bunch of txt.gz files, which are already normalized. So these are not 'raw' files. Let's skip getting the data / normalizing and just jump ahead with GEOquery.
+# it appears that when you dowload GSE7316_RAW.tar from GEO, you actually get a bunch of txt.gz files, which are already normalized. 
+# So these are not 'raw' files. Let's skip getting the data / normalizing and just jump ahead with GEOquery.
 
-#setwd('~/r/final_proj_affy_data')
+#setwd(wd)
 
-geo_7329 <- getGEO('GSE7329')
+geo_7329 <- getGEO('GSE7329') # data might already be in log format, but check with GEO database annotations - SJ
 exprs_geo_7329 <- exprs(geo_7329[[1]])
+
 
 #get the experimental conditions for ANOVA?
 file_designations_2 = colnames(exprs_geo_7329)
@@ -62,17 +69,49 @@ anova_7329.bfn = p.adjust(anova_7329, method="bonferroni")
 
 test <- which(anova_7329 < .05)
 length(test) # ~10,000 elements with signficant p-values, let's see if we can narrow this down somehow
-# shoudl p-values be adjusted for multiple-hypothesis testing?
+# should p-values be adjusted for multiple-hypothesis testing? - SJ
 
-# check adjusted p-value
+# check adjusted p-value - SJ
 test.fdr <- anova_7329.fdr[anova_7329.fdr < 0.05]
 test.bfn <- anova_7329.bfn[anova_7329.bfn < 0.05]
 length(test.fdr) # approximately 3882
 length(test.bfn) # approximately 149
 
-# get gene names
+# get gene names - SJ
 genes.fdr <- names(test.fdr)
 genes.bfn <- names(test.bfn)
-# what are the gene_ids?  Can we convert them to genbank symbols?
+# what are the gene_ids?  Can we convert them to genbank symbols? - SJ
+
+# get log fold change using MKs getRange function - SJ
+getRange <- function(expvalues, expgroups) {
+    range.results = range(tapply(expvalues, expgroups, mean))
+    rangediff = range.results[2]-range.results[1]
+}
+
+range.results = apply(exprs_geo_7329, 1, getRange, gse5634_expdesign) # assume exprs_geo_7329 is already log normalized
+# finally get the results
+diffexp_gse5634 = logexprs_gse5634[range.results > 5 & anova.results.fdr
+                                   < 0.05,]
 
 
+#####
+# get gse25507
+download.file("http://www.ncbi.nlm.nih.gov/geo/download/?acc=GSE25507&format=file", destfile="gse25507.tar")
+untar("gse25507.tar", exdir="gse25507")
+
+setwd("gse25507")
+
+#begin processing data
+gse25507.raw <- ReadAffy(compress=TRUE, verbose=TRUE)
+gse25507.norm <- gcrma(gse25507.raw)
+gse25507.exprs <- exprs(gse25507.norm)
+
+
+geo25507 <- getGEO("GSE25507")
+geo25507.exprs <- exprs(geo25507[[1]])
+file_designations.25507 = colnames(gse25507.exprs)
+design.25507 = vector(length=length(file_designations.25507))
+for (i in 1:length(file_designations.25507)) {
+    gsm = getGEO(GEO=strsplit(file_designations.25507[i], split="[.]")[1])
+    design.25507[i] = Meta(gsm)$characteristics_ch1
+}
